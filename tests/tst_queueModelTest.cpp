5e4572720ea79d21d2b33f1f3ee2621912ab810f
Dugan Chen <thedoogster@gmail.com>
2021-05-08 11:39:25 -0700

    Declare a mock song

--- tests/tst_queueModelTest.cpp
+++ tests/tst_queueModelTest.cpp
@@ -1,8 +1,14 @@
-#include "mpdclient/connection.h"
+#include "mpdclient/song.h"
 #include "queuemodel.h"
 #include "gmock/gmock.h"
 #include <QObject>
 #include <QTest>
+
+class MockSong : public mpd::Song
+{
+public:
+    MOCK_METHOD(const char *, get_tag, (mpd_tag_type, unsigned), (override));
+};
 
 class TestQueueModel : public QObject
 {
