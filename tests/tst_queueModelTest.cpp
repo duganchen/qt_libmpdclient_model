986b35ebacca83800a9edf43bbfd504cc4963413
Dugan Chen <thedoogster@gmail.com>
2021-05-08 13:03:22 -0700

    Add a test for the queue model

--- tests/tst_queueModelTest.cpp
+++ tests/tst_queueModelTest.cpp
@@ -1,8 +1,14 @@
+#include "mpdclient/connection.h"
 #include "mpdclient/song.h"
 #include "queuemodel.h"
 #include "gmock/gmock.h"
+#include <mpd/client.h>
+#include <vector>
 #include <QObject>
 #include <QTest>
+
+using ::testing::ByMove;
+using ::testing::Return;
 
 class MockSong : public mpd::Song
 {
@@ -10,11 +16,34 @@ public:
     MOCK_METHOD(const char *, get_tag, (mpd_tag_type, unsigned), (override));
 };
 
+class MockConnection : public mpd::Connection
+{
+public:
+    MOCK_METHOD(std::vector<std::unique_ptr<mpd::Song>>, list_queue_meta, (), (override));
+    MOCK_METHOD(mpd_error, get_error, (), (override));
+    explicit operator bool() override { return true; };
+};
+
 class TestQueueModel : public QObject
 {
     Q_OBJECT
 private slots:
-    void aTest() { QCOMPARE(1, 1); }
+    void queueModelTest()
+    {
+        std::cout << "aaaaa\n";
+        std::unique_ptr<MockSong> song(new MockSong());
+        EXPECT_CALL(*song, get_tag(MPD_TAG_TITLE, 0)).WillOnce(Return("Song title"));
+        std::vector<std::unique_ptr<mpd::Song>> songs;
+        songs.push_back(std::move(song));
+
+        MockConnection connection;
+        EXPECT_CALL(connection, get_error()).WillOnce(Return(MPD_ERROR_SUCCESS));
+        EXPECT_CALL(connection, list_queue_meta()).WillOnce(Return(ByMove(std::move(songs))));
+
+        QueueModel model(connection);
+        model.refresh();
+        QCOMPARE(model.data(model.index(0, 0)), QString("Song title"));
+    }
 };
 
 QTEST_MAIN(TestQueueModel)
